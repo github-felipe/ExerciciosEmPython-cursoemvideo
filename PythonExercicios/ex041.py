876a7a13321ac6c648610fccb02737c73b725b3c
Felipe Lima <felipe.lima.eu@hotmail.com>
2020-11-30 15:46:04 -0300

    correção

--- PythonExercicios/ex041.py
+++ PythonExercicios/ex041.py
@@ -1,12 +1,16 @@
-idade = int(input('\033[30mDigite a idade do(a) atleta: \033[m'))
+from datetime import datetime
+atual = datetime.today().year
+nascimento = int(input('\033[30mDigite o ano de nascimento do(a) atleta: \033[m'))
+print(f'Ano de nascimento: {nascimento}')
+idade = atual - nascimento
 if idade <= 9:
-    print(f'\033[30mCom a idade de {idade} anos este atleta é considerado(a) MIRIM\033[m')
+    print(f'\033[30mEste atleta tem {idade} anos. Classificação: MIRIM\033[m')
 elif idade <= 14:
-    print(f'\033[30mCom a idade de {idade} anos este atleta é considerado(a) INFANTIL\033[m')
+    print(f'\033[30mEste atleta tem {idade} anos. Classificação: INFANTIL\033[m')
     print('\033[37mVê se amadurece')
 elif idade <= 19:
-    print(f'\033[30mCom a idade de {idade} anos este atleta é considerado(a) JUNIOR\033[m')
-elif idade <= 20:
-    print(f'\033[30mCom a idade de {idade} anos este atleta é considerado(a) SÊNIOR\033[m')
+    print(f'\033[30mEste atleta tem {idade} anos. Classificação: JUNIOR\033[m')
+elif idade <= 25:
+    print(f'\033[30mEste atleta tem {idade} anos. Classificação: SÊNIOR\033[m')
 else:
-    print(f'\033[30mCom a idade de {idade} anos este atleta é considerado(a) MASTER\033[m')
+    print(f'\033[30mEste atleta tem {idade} anos. Classificação: MASTER\033[m')
