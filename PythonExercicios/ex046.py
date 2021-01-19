b2d3e97cb259d55c94537738548ea77474568836
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-19 17:33:23 -0300

    adição de melhorias para um exercício

--- PythonExercicios/ex046.py
+++ PythonExercicios/ex046.py
@@ -1,7 +1,12 @@
+import pygame
 from time import sleep
 from emoji import emojize
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('ex046.mp3')
 print('Os show de fogos de artifício começará em:')
 for c in range(10, -1, -1):
     print(str(c) + '...')
     sleep(1)
+pygame.mixer.music.play(loops=0, start=0)
 print(emojize('COMEÇOU!!! :fireworks: :fireworks: :fireworks: :fireworks:', use_aliases=True))
