3545dc6bc410a5bf939618ca6e115a73a9c0b185
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-20 17:52:21 -0300

    melhorias ex055
    uma diminuição e simplificação desse código baseado no vídeo de correção deste exercício

--- PythonExercicios/ex055.py
+++ PythonExercicios/ex055.py
@@ -1,15 +1,12 @@
 print('Obs: troque a vírgula "," por um ponto "."')
-n1 = float(input('Digite o peso de uma pessoa: '))
-maior = 0
-menor = n1
-for c in range(0,4):
-    if n1 > maior:
-        maior = n1
-    if n1 < menor:
-        menor = n1
-    n1 = float(input('Digite o peso de outra pessoa: '))
-if n1 > maior:
-    maior = n1
-if n1 < menor:
-    menor = n1
+for p in range(1, 6):
+    peso = float(input(f'Digite o peso da {p}ª pessoa: '))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
 print(f'O maior peso digitado é: {maior}Kg. e o menor é {menor}Kg.')
