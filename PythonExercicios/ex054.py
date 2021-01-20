a62d2d2a07dd1b6c1b131234aec994b5df3b4f21
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-20 17:35:01 -0300

    melhoria no ex054
    apenas algumas melhorias baseado no vídeo da resolução desse exercício e com o adicional de uma mensagem de erro que eu pensei em colocar para se caso o usuário escrever algum valor maior que o ano atual da máquina

--- PythonExercicios/ex054.py
+++ PythonExercicios/ex054.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 ano = datetime.today().year
-n1 = int(input('Digite o ano de nascimento de uma pessoa: '))
-n2 = 0
-for c in range(0, 6):
-    if ano - n1 >= 21:
-        n2 = n2+ 1
-    n1 = int(input('Digite o ano de nascimento de outra pessoa: '))
-if ano - n1 >= 21:
-    n2 = n2 + 1
-print(f'Um total de {n2} das pessoas digitadas já tem ou terão nesse ano a maioridade')
+totmaior = 0
+totmenor = 0
+for c in range(1, 8):
+    n = int(input(f'Digite o ano de nascimento da {c}ª pessoa: '))
+    if n > ano:
+        print('foi digitado um valor inválido')
+        erro = n + 'mensagem de erro pois foi escrito um valor maior que o ano atual'
+    if ano - n >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print(f'Ao todo foram digitadas {totmaior} pessoas maiores de idade e {totmenor} pessoas menores de idade.')
+print('Obs: considerando o padrão estadunidense de 21 anos para a maioridade')
