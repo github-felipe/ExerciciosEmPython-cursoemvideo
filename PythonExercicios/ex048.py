18a09ee2f571edc0c1d79b2e8a7cbf2799084b55
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-20 15:29:34 -0300

    obs
    uma observação de mudança neste código

--- PythonExercicios/ex048.py
+++ PythonExercicios/ex048.py
@@ -3,3 +3,4 @@ for c in range(3, 501, 6):
     n = c
     valor = valor + n
 print(f'A soma dos números ímpares, múltiplos de 3 entre 1 e 500 é {valor}')
+#dava pra ter feito substituindo (valor = valor + n) por (valor += n)
