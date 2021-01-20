99a4b01568471ea190484b5fd3462bdb7e057bcd
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-20 16:20:24 -0300

    melhorias
    apenas umas pequenas melhorias na resolução deste exercício

--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -1,7 +1,12 @@
+cont = 0
 n = 0
-for c in range(0, 6):
-    x = int(input('Digite um número inteiro: '))
+for c in range(1, 7):
+    x = int(input(f'Digite o {c}° número inteiro: '))
     n2 = x / 2
     if n2.is_integer():
-        n = n + x
-print(f'A soma de todos os números pares que você digitou é: {n}')
+        n += x
+        cont += 1
+if cont == 1:
+    print(f'Você informou um número par que é: {n}')
+else:
+    print(f'Você informou {cont} números PARES e a soma deles é: {n}')
