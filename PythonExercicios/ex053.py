6f053e164c96137bda4be79981f7e390a7057ea8
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-20 17:14:22 -0300

    melhoria
    uma melhoria no nome das variáveis pra ficar mais autoexplicativo

--- PythonExercicios/ex053.py
+++ PythonExercicios/ex053.py
@@ -1,14 +1,14 @@
 from time import sleep
-frase1 = str(input('Digite uma frase sem acentos: ')).strip()
+frase = str(input('Digite uma frase sem acentos: ')).strip()
 print('Verificando se ela é um palíndromo... ')
 sleep(0.3)
-frase2 = frase1.replace(' ', '').lower()
-frase3 = ''
-n1 = len(frase2)
+semEsp = frase.replace(' ', '').lower()
+contrario = ''
+n1 = len(semEsp)
 for c in range(n1-1, -1, -1):
-    frase3 = frase3 + f'{frase2[c]}'
-if frase3 == frase2:
-    print(f'"{frase1}" é um palíndromo!')
+    contrario += f'{semEsp[c]}'
+if contrario == semEsp:
+    print(f'"{frase}" é um palíndromo!')
 else:
-    print(f'{frase1} não é um palíndromo!')
+    print(f'{frase} não é um palíndromo!')
 #também poderia ser feito assim: frase3 = frase2[::-1]
