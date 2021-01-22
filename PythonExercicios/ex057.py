f13318f8a33ff0ace15b39ab81f26bdf6bca8759
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-22 16:26:04 -0300

    melhoria
    uma melhoria nesse código baseado no vídeo de correção deste exercício que consiste em: se o usuário digitar o gênero por extenso ao invés da primeira letra o programa ainda irá aceitar baseada nessa mesma primeira letra

--- PythonExercicios/ex057.py
+++ PythonExercicios/ex057.py
@@ -1,6 +1,6 @@
 gen = 'a'
 while gen not in 'MF':
-    gen = str(input('Digite seu gênero [M/F]: ')).strip().upper()
+    gen = str(input('Digite seu gênero [M/F]: ')).strip().upper()[0]
     if gen not in 'MF':
         print('O valor inserido está incorreto. Tente novamente.')
 print(f'O gênero selecionado foi: {gen}')
