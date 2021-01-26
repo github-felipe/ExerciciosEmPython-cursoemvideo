ed2038e42c9953aea87b93178adfb25e98fbc8a8
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-26 18:55:35 -0300

    ex060
    A correção de um problema que eu não tinha reparado anteriormente e uma dica dada durante a aula de resolução do exercício de uma forma mais prática para calcular o fatorial

--- PythonExercicios/ex060.py
+++ PythonExercicios/ex060.py
@@ -1,16 +1,22 @@
 n1 = n2 = int(input('Digite um número para ver seu fatorial: '))
-fat = 1
+f = 1
 quant = 0
 while quant != n1:
     quant += 1
-    fat *= n2
-    print(f'\033[37m{n2}*', end='')
+    f *= n2
+    print(f'\033[37m{n2}', end='')
+    print('*' if n2 > 1 else '= ', end='')
     n2 -= 1
-print(f'= \033[m{fat}')
+print(f'\033[m{f}')
 
 n1 = int(input('Digite outro número para ver seu fatorial: '))
-fat = 1
+f = 1
 for c in range(n1, 0, -1):
-    fat *= c
-    print(f'\033[37m{c}*', end='')
-print(f'= \033[m{fat}')
+    f *= c
+    print(f'\033[37m{c}', end='')
+    print('*' if c > 1 else '= ', end='')
+print(f'\033[m{f}')
+"""Também pode ser feito da seguinte forma:
+from math import factorial
+n = int(input('Digite um número'))
+print(f'O fatorial de {n} é: {factorial(n)}')"""
