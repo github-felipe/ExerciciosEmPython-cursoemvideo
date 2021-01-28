f1cd37f067d1bc362a7e8227b6478c65b5316e8e
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-28 19:56:01 -0300

    ex070
    O objetivo desse exercício foi criar um programa aonde o usuário digita o nome do produto, o valor e após terminar de digitar os produtos que desejar o programa para. Após isso aparece o valor total da compra, quantos produtos custam mais de R$:1000,00 e qual é o produto mais barato.

---
+++ PythonExercicios/ex070.py
@@ -0,0 +1,29 @@
+contador = total = maisDeMil = 0
+print(f'''{"-" * 30}
+{'CASAS VALIA':^30}
+{"-" * 30}''')
+while True:
+    continuar = 'a'
+    contador += 1
+    nome = str(input('Digite o nome do produto: '))
+    valor = float(input('Digite o valor do produto R$:'))
+    if contador == 1:
+        maisBarato = nome
+        valorMaisBaixo = valor
+    else:
+        if valor < valorMaisBaixo:
+            maisBarato = nome
+            valorMaisBaixo = valor
+        elif valor == valorMaisBaixo:
+            maisBarato = maisBarato + ',', nome
+    total += valor
+    if valor > 1000:
+        maisDeMil += 1
+    while continuar not in 'SN':
+        continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+    if continuar == 'N':
+        break
+print(f'''O valor total da compra é: R${total}
+Um total de {maisDeMil} produtos selecionados custam mais de R$:1000.00
+O produto mais barato é {maisBarato}, custando R${valorMaisBaixo}
+Obrigado pela preferência, volte sempre!''')
