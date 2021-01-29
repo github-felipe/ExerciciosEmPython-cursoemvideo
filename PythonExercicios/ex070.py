585bf865ddd74b770d82289e69d12d47eef2a8d7
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-01-29 18:14:25 -0300

    Update ex070.py
    coloquei uma melhoria baseada na aula que me fez diminuir algumas linhas e eu adicionei linhas vazias pra melhorar a legibilidade do código

--- PythonExercicios/ex070.py
+++ PythonExercicios/ex070.py
@@ -2,28 +2,29 @@ contador = total = maisDeMil = 0
 print(f'''{"-" * 30}
 {'CASAS VALIA':^30}
 {"-" * 30}''')
+
 while True:
     continuar = 'a'
     contador += 1
     nome = str(input('Digite o nome do produto: '))
     valor = float(input('Digite o valor do produto R$:'))
-    if contador == 1:
+
+    if contador == 1 or valor < valorMaisBaixo:
         maisBarato = nome
         valorMaisBaixo = valor
-    else:
-        if valor < valorMaisBaixo:
-            maisBarato = nome
-            valorMaisBaixo = valor
-        elif valor == valorMaisBaixo:
-            maisBarato = maisBarato + ',', nome
+    elif valor == valorMaisBaixo:
+        maisBarato = maisBarato, nome
+
     total += valor
     if valor > 1000:
         maisDeMil += 1
+
     while continuar not in 'SN':
         continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()
     if continuar == 'N':
         break
-print(f'''O valor total da compra é: R${total}
+
+print(f'''O valor total da compra é: R${total:.2f}
 Um total de {maisDeMil} produtos selecionados custam mais de R$:1000.00
-O produto mais barato é {maisBarato}, custando R${valorMaisBaixo}
+O produto mais barato é: {maisBarato}, custando R${valorMaisBaixo:.2f}
 Obrigado pela preferência, volte sempre!''')
