7ce4d4fd849a3add54c29ad4eef6e0f4c24fa104
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-02 18:56:30 -0300

    ex074
    O objetivo desse exercício era: colocar 5 valores aleatórios em uma tupla, mostrar eles e depois mostrar o maior, e o menor valores sorteados

---
+++ PythonExercicios/ex074.py
@@ -0,0 +1,5 @@
+from random import randint
+sorteados = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print(f'Os valores sorteados são: {sorteados}')
+print(f'O maior valor é: {sorted(sorteados)[-1]}')
+print(f'O menor valor é: {sorted(sorteados)[0]}')
