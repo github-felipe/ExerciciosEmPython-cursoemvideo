416131782262e3c2140f520abdd7fcf8c18d597e
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-03 16:57:34 -0300

    adição
    Adicionei a opção de o usuário  poder permanecer no programa sem ele fechar seguindo a ideia do professor Guanabara durante o vídeo de resolução do exercício.

--- PythonExercicios/ex072.py
+++ PythonExercicios/ex072.py
@@ -1,9 +1,17 @@
 num = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
        'catorze', 'quinze', 'desesseis', 'desessete', 'dezoito', 'dezenove', 'vinte')
-escolha = int(input('Digite um número entre 0 e 20: '))
 while True:
-    if 0 <= escolha <= 20:
+    escolha = int(input('Digite um número entre 0 e 20: '))
+    while True:
+        if 0 <= escolha <= 20:
+            break
+        else:
+            escolha = int(input('Tente novamente... Escolha um número entre [0-20]: '))
+    print(f'O número escolhido é {num[escolha]}')
+    while True:
+        escolha = str(input('Deseja continuar? [S/N]')).upper()
+        if escolha in 'SN':
+            break
+    if escolha == 'N':
         break
-    else:
-        escolha = int(input('Tente novamente... Escolha um número entre [0-20]: '))
-print(f'O número escolhido é {num[escolha]}')
+print('Obrigado por usar nosso programa, volte sempre!')
