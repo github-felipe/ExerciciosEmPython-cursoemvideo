70c4f56ac2a47e3f4b5ab980828cac21f6d37746
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-03 17:53:36 -0300

    obs
    uma observação de uma maneira diferente de resolver esse exercício

--- PythonExercicios/ex074.py
+++ PythonExercicios/ex074.py
@@ -3,3 +3,4 @@ sorteados = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), ran
 print(f'Os valores sorteados são: {sorteados}')
 print(f'O maior valor é: {sorted(sorteados)[-1]}')
 print(f'O menor valor é: {sorted(sorteados)[0]}')
+#Também poderia ser feito com os comandos: max(sorteados)    e    min(sorteados)
