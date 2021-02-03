df20b6ed8285b6d2c679424f6d201e40f22213f5
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-03 18:20:46 -0300

    melhoria
    melhoria para diminuir o código com base no vídeo de resolução desse exercício

--- PythonExercicios/ex075.py
+++ PythonExercicios/ex075.py
@@ -1,5 +1,6 @@
-tupla = (input('Digite um número: '), input('Digite outro número: '), input('Digite outro número: '),
-         input('Digite outro número: '))
+tupla = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite mais um número: ')),
+         int(input('Digite o último número: ')))
+print(f'Os valores digitados foram: {tupla}')
 if 9 in tupla:
     if tupla.count(9) == 1:
         print('O número nove apareceu 1 vez')
@@ -11,20 +12,11 @@ if 3 in tupla:
     print(f'A primeira vez em que o número três foi digitado foi na {tupla.index(3) + 1}ª posição')
 else:
     print('Não foi digitado o número 3')
-quantPares = 0
-pares = ()
-for c in range(0, 4):
-    if int(tupla[c]) % 2 == 0:
-        quantPares += 1
-        par = tuple(tupla[c])
-        pares2 = (pares, par)
-        pares = pares2
-        del pares2
-if quantPares == 0:
-    print('Não foram escritos números pares')
-elif quantPares == 1:
-    print(f'O número par escolhido foi: {pares[0]}')
-elif quantPares == 2:
-    print(f'Os números pares escolhidos foram: {pares[0]} e {pares[1]}')
-else:
-    print(f'Os números pares escolhidos foram: {str(pares).replace("(", "").replace(")", "").replace(",,", ",")[2:-1]}')
+print('Os valores pares digitados foram: ', end='')
+pares = 0
+for num in tupla:
+    if num % 2 == 0:
+        print(num, end=' ')
+        pares += 1
+if pares == 0:
+    print('Nenhum')
