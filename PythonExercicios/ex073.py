fc168ccad59e3df556ce2a2580d9f7d56684f1e7
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-03 17:40:58 -0300

    melhoria
    O objetivo dessa melhoria foi o de tornar o programa mais legível para o usuário

--- PythonExercicios/ex073.py
+++ PythonExercicios/ex073.py
@@ -1,7 +1,11 @@
 classificação = ('Internacional', 'Flamengo', 'Atlético-MG', 'São Paulo', 'Fluminense', 'Palmeiras', 'Grêmio', 'Athletico-PR', 'Ceará SC', 'Corinthians',
                  'Santos', 'Atlético-GO', 'Bragantino', 'Vasco da Gama', 'Bahia', 'Sport Recife', 'Fortaleza', 'Goiás', 'Coritiba', 'Botafogo')
+print('-=' * 20)
 print(f'Os 5 melhores times no brasileirão atual é: {classificação[:5]}')
+print('-=' * 20)
 print(f'Os times que estão no top 4 ao contário são: {classificação[-4:]}')
+print('-=' * 20)
 print(f'Os times em ordem alfabética são: {sorted(classificação)}')
+print('-=' * 20)
 print(f'Chapecoense não está mais na série A...')
 print(f'Já São Paulo está na posição: {classificação.index("São Paulo") + 1}')
