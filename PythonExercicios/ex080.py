e9fc1fa7be3049ab4d4e8c56adf5b19d8b0f560d
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-02-05 18:29:52 -0300

    ex080
    O objetivo desse exercício era fazer um programa aonde o usuário possa inserir 5 números. Após ser inserido um número o programa retornará ao usuário a posição em que este número foi adicionado na lista, respeitando a ordem crescente dos números sem utilizar o sort()

---
+++ PythonExercicios/ex080.py
@@ -0,0 +1,23 @@
+lista = list()
+contador = 0
+for c in range(0, 5):
+    contador += 1
+    inserido = -1
+    n = int(input('Digite um número: '))
+    if contador == 1:
+        inserido = 0
+        lista.append(n)
+    else:
+        for num in lista:
+            if n < num:
+                inserido = lista.index(num)
+                break
+        if inserido == -1:
+            lista.append(n)
+        else:
+            lista.insert(inserido, n)
+    if inserido == -1:
+        print(f'O número {n} foi inserido no final da lista')
+    else:
+        print(f'O número {n} foi inserido na posição {inserido} da lista.')
+print(f'Lista final: {lista}')
