ca5a0fa2eb8992bf466490e9137935ff6614347b
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-03-09 18:35:05 -0300

    arrumando o ex080
    consertando o código para aparecer a mensagem da posição na qual foi inserido na lista o primeiro valor digitado

--- PythonExercicios/ex080.py
+++ PythonExercicios/ex080.py
@@ -4,16 +4,15 @@ for c in range(0, 5):
     n = int(input('Digite um número: '))
     if c == 0:
         inserido = 0
-        lista.append(n)
     else:
         for num in lista:
             if n < num:
                 inserido = lista.index(num)
                 break
-        if inserido == -1:
-            lista.append(n)
-            print(f'O número {n} foi inserido no final da lista.')
-        else:
-            lista.insert(inserido, n)
-            print(f'O número {n} foi inserido na posição {inserido} da lista.')
+    if inserido == -1:
+        lista.append(n)
+        print(f'O número {n} foi inserido no final da lista.')
+    else:
+        lista.insert(inserido, n)
+        print(f'O número {n} foi inserido na posição {inserido} da lista.')
 print(f'Lista final: {lista}')
