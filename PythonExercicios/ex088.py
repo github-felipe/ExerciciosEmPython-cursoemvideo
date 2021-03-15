d76236ed4a57f85fedaa328962bd72ff02d7b40e
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-03-15 15:49:24 -0300

    ex088
    O objetivo desse exercício era criar um programa que sorteia quantos jogos da mega sena o usuário quiser.

---
+++ PythonExercicios/ex088.py
@@ -0,0 +1,21 @@
+from random import choice
+from time import sleep
+print('-='*20)
+print(f'{"JOGA NA MEGA SENA":^40}')
+print('-='*20)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+jogos = list()
+jogo = list()
+valores = 0
+for c in range(1, quant+1):
+    valores = list(range(1, 61))
+    for num in range(0, 6):
+        valor = choice(valores)
+        jogo.append(valor)
+        valores.remove(valor)
+    jogo = sorted(jogo)
+    print(f'Jogo {c}: {jogo}')
+    sleep(1)
+    jogo.clear()
+    valores.clear()
+print('Boa sorte!')
