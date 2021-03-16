2256f5589619d4d2e60c0c4c37b4c5a2d0cb65e0
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-03-16 17:50:34 -0300

    melhoria ex085
    uma leve simplificada no código

--- PythonExercicios/ex085.py
+++ PythonExercicios/ex085.py
@@ -5,7 +5,7 @@ for c in range(1, 8):
         numeros[0].append(n)
     else:
         numeros[1].append(n)
-    numeros[0] = sorted(numeros[0])
-    numeros[1] = sorted(numeros[1])
+    numeros[0].sort()
+    numeros[1].sort()
 print(f'''A lista de número pares é: {numeros[0]}
 A lista de números ímpares é: {numeros[1]}''')
