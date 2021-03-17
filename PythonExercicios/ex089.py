36d1d06f75b0d04b3e9509aabdad6da4a2169507
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-03-17 14:19:43 -0300

    leve melhoria ex089
    uma simplificação no código diminuindo as linhas 8 e 9 para apenas 1 linha e uma leve melhoria na estética do programa ao usuário

--- PythonExercicios/ex089.py
+++ PythonExercicios/ex089.py
@@ -5,8 +5,7 @@ while True:
     nome = str(input('Nome: '))
     n1 = float(input('Nota 1: '))
     n2 = float(input('Nota 2: '))
-    alunos.append([nome])
-    alunos[contador].append([n1, n2])
+    alunos.append([nome, [n1, n2]])
     média.append((n1 + n2) / 2)
     while True:
         continuar = str(input('Deseja continuar? [S/N]: ').upper())
@@ -17,20 +16,23 @@ while True:
     if continuar == 'N':
         break
     contador += 1
-print('-=' * 10)
+print('-=' * 11)
 print('No. Nome        Média')
-print('_'*21)
+print('_'*22)
 contador = 0
 for aluno in alunos:
-    print(f'{contador:<3} {aluno[0]:<11} {média[contador]:.2f}')
+    print(f'{contador:<3} {aluno[0]:<11} {média[contador]:.1f}')
     contador += 1
-print('-=' * 10)
+print('-=' * 11)
 while True:
     escolha = int(input('Você deseja ver as notas de qual aluno? (utilize o número dele na tabela) (999 para parar) '))
     if escolha == 999:
         break
-    print(f'''Aluno: {alunos[escolha][0]}
+    if escolha < len(alunos):
+        print(f'''Aluno: {alunos[escolha][0]}
 Nota 1: {alunos[escolha][1][0]}
 Nota 2: {alunos[escolha][1][1]}
 Média: {média[escolha]}''')
+    else:
+        print('Opção inválida, tente novamente...')
 print('Muito obrigado por utilizar o nosso programa, volte sempre!!!')
