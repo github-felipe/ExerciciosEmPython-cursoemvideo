20dbbcd048ee0f30ca2dd9bc10175b1b6c3a5d32
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-03-30 12:40:45 -0300

    ex097
    O objetivo desse exercício era criar um programa com a função escreva() aonde ela recebe apenas um parâmetro no qual ela irá mostrar na tela uma mensagem contendo esse mesmo parâmetro só que com  uns enfeites na linha de cima e de baixo

---
+++ PythonExercicios/ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    print('~' * (len(msg) + 2))
+    print(f' {msg} ')
+    print('~' * (len(msg) + 2))
+
+
+escreva('Olá, mundo!')
+escreva('Felipe Lima')
+escreva('pneumoultramicroscopicossilicovulcanoconiótico')
+escreva('Curso em Vídeo')
+escreva('a')
