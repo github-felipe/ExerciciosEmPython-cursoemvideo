1d27fbd418b613ca5ea62e8bb9d2f8a80426bb6d
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-04-07 17:14:51 -0300

    ex101
    O objetivo desse exercício era criar um programa com uma função que calcula a idade da pessoa(recebendo o ano de nascimento) e retorne uma string com uma mensagem dizendo se nessa idade o voto é negado, opicional ou obrigatório

---
+++ PythonExercicios/ex101.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+hoje = datetime.today().year
+
+
+def voto(nascimento):
+    idade = hoje - nascimento
+    if idade < 16:
+        return f'Idade {idade}: Voto NEGADO'
+    elif 15 < idade < 18:
+        return f'Idade {idade}: Voto OPICIONAL'
+    elif 65 > idade > 17:
+        return f'Idade {idade}: Voto OBRIGATÓRIO'
+    elif idade > 64:
+        return f'Idade {idade}: Voto OPICIONAL'
+
+
+print('-' * 30)
+nasceu = int(input('Digite seu ano de nascimento: '))
+print(voto(nasceu))
