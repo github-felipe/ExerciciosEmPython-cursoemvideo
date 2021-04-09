4a1459d033fba3a2218f53adf74672d91e1ebb07
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-04-09 14:24:34 -0300

    correção ex103
    corrigindo o erro aonde o usuário pode colocar espaços antes ou depois de começar a escrever nos dois inputs do programa

--- PythonExercicios/ex103.py
+++ PythonExercicios/ex103.py
@@ -2,8 +2,8 @@ def ficha(nome='<desconhecido>', gols=0):
     return f'O jogador {nome} fez {gols} gols no campeonato.'
 
 
-nom = str(input('Nome: '))
-gol = str(input('Gols: '))
+nom = str(input('Nome: ')).strip()
+gol = str(input('Gols: ')).strip()
 if len(nom) > 0 and gol.isnumeric():
     gol = float(gol)
     if gol.is_integer():
