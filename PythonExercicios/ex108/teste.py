f7e7892e341e51397af1d254feddf4b4a790e38b
Felipe Lima <felipe.lima.eu@hotmail.com>
2021-04-13 20:03:50 -0300

    patch ex108
    corrigindo a mostragem de resultados aonde na linha número '6' não utilizada a função moeda()

--- PythonExercicios/ex108/teste.py
+++ PythonExercicios/ex108/teste.py
@@ -3,5 +3,5 @@ from ex108 import moeda
 p = float(input('Digite o preço R$:'))
 print(f'{moeda.moeda(p)} mais 10% = {moeda.moeda(moeda.aumentar(p, 10))}')
 print(f'{moeda.moeda(p)} menos 13% = {moeda.moeda(moeda.diminuir(p, 13))}')
-print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p):.2f}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
 print(f'O dobro de {moeda.moeda(p)} é  {moeda.moeda(moeda.dobro(p))}')
